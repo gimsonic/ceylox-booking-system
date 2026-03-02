366487568047f803a86389965300eba96dc69be5
Gimhana Menake <gimhanamenaka@icloud.com>
2026-03-02 13:48:38 +0530

    feat(backend): add USER and BOOKING schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,16 +1,61 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
+from uuid import UUID     #UUID : for professional ID management
+from datetime import date
 
-# Define what needed when a user CREATES a service
-class ServiceCreate(BaseModel):
+# ---- SERVICES ----
+class ServiceBase(BaseModel):
     name: str
     description : Optional[str] = None
     price: float
     duration_minutes: int
+    img_url: Optional[str] = None
+
+class ServiceCreate(ServiceBase):
+    pass
 
 # Defines what a service looks like when SEND it back to the user
 class Service(ServiceCreate):
     id: int
 
+    class Config:
+        from_attributes = True   #Allows Pyddantic to read data from the database
+
+
+# ---- USER ----
+class UserBase(BaseModel):
+    full_name: str
+    email : EmailStr
+    mobile : str | None = None
+
+class UserCreate(UserBase):
+    '''Used for the 'Lazy Registration' process'''
+    pass
+
+class User(UserCreate):
+    id : UUID
+
+    class Config:
+        from_attributes = True
+
+# ---- BOOKING ----
+class BookingBase(BaseModel):
+    room_id: int
+    check_in_date: date
+    check_out_date: date
+    total_price: Optional[float] = None
+
+class BookingCreate(BookingBase):
+    ''' Combines room data and user data to facilitate a one-step checkout process '''
+    full_name: str
+    email: EmailStr
+    mobile: Optional[str] = None
+
+class Booking(BookingCreate):
+    # The complete Booking object as stored in thr DB
+    id: UUID
+    user_id: UUID # Link the BOOKING back with USER table
+    status: str = "confirmed"
+    
     class Config:
         from_attributes = True
